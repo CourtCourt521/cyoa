cce16c39cbaf719d56028095470da472f3727fd7
Courtney Ruhm <cruhm@cruhm.remote.csb>
2022-12-12 16:23:35 -0700

    deleted:    cyoastory/cyoa 	new file:   cyoastory/cyoa.go 	modified:   main.go

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"github.com/CourtCourt521/cyoa/cyoastory"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "The port to start the server")
 	filename := flag.String("file", "gopher.json", "The JSON file with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s. \n", *filename)
@@ -17,11 +22,89 @@ func main() {
 		panic(err)
 	}
 
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	tpl := template.Must(template.New("").Parse(storyTmpl))
+
+	h := cyoa.NewHandler(story,
+		cyoa.WithTemplate(tpl),
+		cyoa.WithPathFunc(pathFn),
+	)
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+	fmt.Printf("Starting the server on port %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story/"):]
+}
+
+var storyTmpl = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta   charset="utf-8">
+        <title>Choose Your Own Adventure</title>
+    </head>
+    <body> 
+	  <section class="page">
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+          <p>{{.}}</p>
+        {{end}}
+        <ul>
+        {{range .Options}}
+          <li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+	  </section>
+	  <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FFFCF6;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #7792a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+    </body>
+</html>`
